e4cf74e22e7a205d55d90c0be3b31403f921547c
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-07-15 22:38:12 +0200

    feat: started on map generator

---
+++ Assets/Code/CorrectZIndex.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+* Assume map starts at y = 0 and increases with y, and that each wall has it's sortingOrder = y
+*/
+
+public class CorrectZIndex : MonoBehaviour
+{
+    public bool updatePerFrame = true;
+
+    private void LateUpdate()
+    {
+
+    }
+}
