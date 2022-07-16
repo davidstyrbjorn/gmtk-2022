bf53b7383e57d83d5ba415260b7eb21354662056
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-07-16 10:37:17 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Code/Health.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int hp;
+    public int invinsibilityTimer = 0;
+    private bool isInvinsible = false;
+
+    public void TakeDamage(int howMuch)
+    {
+        if (isInvinsible) return;
+
+        // Is there a timer for how long we are invinsible? Become invinsible for X seconds
+        if (invinsibilityTimer != 0)
+        {
+            isInvinsible = true;
+            StartCoroutine("Invinsibility");
+        }
+
+        hp -= howMuch;
+    }
+
+    private IEnumerator Invinsibility()
+    {
+        yield return new WaitForSeconds(invinsibilityTimer);
+        isInvinsible = false;
+    }
+}
