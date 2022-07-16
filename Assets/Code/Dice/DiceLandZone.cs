7edb992d880af5c9736eb57ec8431d9e5c6da5a6
Andreas Lasses <andreas.lasses@gmail.com>
2022-07-16 10:41:29 +0200

    Dice land zone, dice values, all in gym, higher walls to prevent dice from going outside. locking dice, outline on dice

---
+++ Assets/Code/Dice/DiceLandZone.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceLandZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerStay(Collider other)
+	{
+        DieSide dieSide = other.GetComponent<DieSide>();
+        if (dieSide != null)
+        {
+            Die die = other.GetComponentInParent<Die>();
+            float epsilon = 0.001f;
+            if (die.dieVelocity.magnitude < epsilon)
+            {
+                die.value = 7 - dieSide.value;
+            }
+        }
+    }
+}
