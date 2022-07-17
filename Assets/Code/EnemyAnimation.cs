ae9e6db24bedb17c25d786a4ebb40c5865e7dded
Viktor Carlsson <99viktor.vc@gmail.com>
2022-07-17 14:57:54 +0200

    Actually fixed the bugs this time

--- Assets/Code/EnemyAnimation.cs
+++ Assets/Code/EnemyAnimation.cs
@@ -42,7 +42,7 @@ public class EnemyAnimation : MonoBehaviour
     IEnumerator Attack()
     {
         animator.SetBool("isAttacking", true);
-        yield return new WaitForSeconds(0.6f);
+        yield return new WaitForSeconds(0.2f);
         animator.SetBool("isAttacking", false);
     }
 }
