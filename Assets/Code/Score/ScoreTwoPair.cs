5f9c321792ebee2e18444e36fabd81703204a896
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-07-17 13:09:40 +0200

    changed literal of twopairs

--- Assets/Code/Score/ScoreTwoPair.cs
+++ Assets/Code/Score/ScoreTwoPair.cs
@@ -6,7 +6,6 @@ public class ScoreTwoPair : ScoreFunction
 {
     const float TWO_PAIR_MAX = 22;
 
-
     public override int CountScore(int[] results)
     {
         score = 0;
@@ -23,7 +22,7 @@ public class ScoreTwoPair : ScoreFunction
 
         if (pairOne != 0 && pairTwo != 0) score = pairOne * 2 + pairTwo * 2;
 
-        string s = $@"<align=left>Two of a kind<line-height=0>
+        string s = $@"<align=left>Two pairs<line-height=0>
         <align=right>{score.ToString()}<line-height=1em>";
         GetComponent<TMPro.TextMeshProUGUI>().SetText(s);
 
