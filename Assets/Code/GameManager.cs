aeccc05130611bbc50d568388d0a852626c472dd
davidstyrbjorn <davidstyrbjorn@gmail.com>
2022-07-17 18:32:01 +0200

    pause mechanic

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -112,6 +112,11 @@ public class GameManager : MonoBehaviour
 
         if (playerController.deathFlag) return;
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+
         // string text = " Barfight number: " + (timesGambled + 1).ToString();
         // text += "\n EnemySpawnRateMultiplier: " + scale.GetEnemySpawnRateMultiplier();
         // text += "\n EnemyMoveSpeedMultiplier: " + scale.GetEnemyMovementMultiplier();
@@ -175,6 +180,22 @@ public class GameManager : MonoBehaviour
         timesGambled++;
         gameState = GAME_STATE.GAMBLING;
         Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void TogglePause()
+    {
+        bool doPause = Time.timeScale == 1;
+
+        if (doPause)
+        {
+            FindObjectOfType<GunBehavior>().enabled = false;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            FindObjectOfType<GunBehavior>().enabled = true;
+            Time.timeScale = 1;
+        }
     }
 
     public void OnGamblingOver()
